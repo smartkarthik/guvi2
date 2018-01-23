fb750164be0dc1646af72861be61f74565d76586
smartkarthik <35652317+smartkarthik@users.noreply.github.com>
2018-01-23 17:06:44 +0530

    Create g.c

---
+++ g.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main()
+{
+    int a=5;
+    if(a>0)
+    printf("Given number is positive");
+    else if(a==0)
+    printf("number is zero");
+    else
+    printf(Number is negative");
+}
